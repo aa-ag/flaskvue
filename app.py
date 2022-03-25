970f6546c4077e683af0b3a75936773e7570aeb2
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-24 21:01:14 -0500

    Add hello_world route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 ############------------ IMPORTS ------------############
+from crypt import methods
 from flask import Flask, jsonify
 from flask_cors import CORS
+from itsdangerous import json
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
@@ -14,7 +16,11 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+
 ############------------ FUNCTION(S) ------------############
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return jsonify('world')
 
 
 ############------------ DRIVER CODE ------------############
