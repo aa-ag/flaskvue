b41c9c04b4effb7d8452dc42451ba466b9e8b8bd
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-25 22:02:55 -0500

    Fix component name bug, working as expected

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../components/HelloWorld.vue';
+import HelloWorld from '../components/HelloWorld.vue';
 
 Vue.use(VueRouter);
 
-export default new Router({
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/hello',
       name: 'hello',
-      componen: HelloWorld,
+      component: HelloWorld,
     },
   ],
 });
