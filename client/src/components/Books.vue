2df4e71d171cc5a81a8f9ff6433b609f502b9e9a
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-27 20:18:19 -0500

    Add row & col with h1 Books and button to add books

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
-      <p>this is working bb!</p>
+      <div class="row">
+        <div class="col-sm-10">
+          <h1>Books</h1>
+          <hr><br><br>
+          <button type="button" class="btn btn-success btn-sm">
+            Add Book
+          </button>
+        </div>
+      </div>
   </div>
 </template>
 <script>
