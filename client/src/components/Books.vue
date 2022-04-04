44a0d8aa7fa6ef896e6dbb2ccf7ca916fcb59e20
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-03 19:39:24 -0500

    Add form group for "read?" input

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -98,6 +98,11 @@
                     placeholder="Enter author">
       </b-form-input>
     </b-form-group>
+    <b-form-group id="form-read-edit-group">
+      <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+        <b-form-checkbox value="true">Read?</b-form-checkbox>
+      </b-form-checkbox-group>
+    </b-form-group>
   </b-form>
 </b-modal>
   </div>
