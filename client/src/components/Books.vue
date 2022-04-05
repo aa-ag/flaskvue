b8f2581f0ff552d539e84b0e711f41ccf7e344c0
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-04 22:10:17 -0500

    Add editBook to editForm

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -136,6 +136,9 @@ export default {
         title: '',
         author: '',
         read: [],
+        editBook(book) {
+          this.editForm = book;
+        }
       },
       message: '',
       showMessage: false,
