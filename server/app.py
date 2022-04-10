e961b4b7e09cb24d255bb7f0a192b060e6d75f28
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-09 22:15:09 -0500

    Add spacing on single_book for readability

--- server/app.py
+++ server/app.py
@@ -66,17 +66,22 @@ def list_all_books():
 
 @app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
 def single_book(book_id):
+    
     response_object = {'status': 'success'}
+
     if request.method == 'PUT':
         post_data = request.get_json()
         remove_book(book_id)
+        
         BOOKS.append({
             'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'author': post_data.get('author'),
             'read': post_data.get('read')
         })
+        
         response_object['message'] = 'Book updated!'
+
     return jsonify(response_object)
 
 
