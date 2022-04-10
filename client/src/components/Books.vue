cc22349f5670aa346b33f59eb9b7afe767b794ee
aa-ag <aaron.aguerrevere@gmail.com>
2022-04-09 22:34:06 -0500

    Fix console missing semi colon from removeBook

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -199,7 +199,7 @@ export default {
         })
         .catch((error) => {
           // eslint-disable-next-line
-          console.error(error)
+          console.error(error);
           this.getBooks();
         });
     },
